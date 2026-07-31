3d06c3087657148ec7f183aecab127b877daca19
Pedro Santo <pedro.bb.90@gmail.com>
2026-07-31 17:59:19 +0100

    update

--- main.go
+++ main.go
@@ -2,32 +2,50 @@ package main
 
 import (
 	"fmt"
-	"os"
+	// "os"
+	"context"
+	"log"
+
+	"github.com/chromedp/chromedp"
 )
 
 func main() {
 	fmt.Println("Welcome SVG Auto!")
 	
-	fmt.Println(getFiles())
-}
-
-func getFiles() []string {
-	var args []string
-	
-	for _, arg := range os.Args[1:] {
-		args = append(args, arg)
-	}
-	return args
+	// fmt.Println(getFiles())
+	accessIcoMoon()
 }
 
 func accessIcoMoon() {
-	// this function should access icomoon.io
+	url := "https://icomoon.io/app/#/select"
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	err := chromedp.Run(ctx,
+			chromedp.Navigate(url),
+			chromedp.WaitVisible("body"),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func uploadFiles() {
-	// this function should updload files to icommon.io
-}
+// func getFiles() []string {
+// 	var args []string
+// 	
+// 	for _, arg := range os.Args[1:] {
+// 		args = append(args, arg)
+// 	}
+// 	return args
+// }
 
-func downloadFiles() {
-	// this function should download files from iconmoon.io
-}
+// this function should access icomoon.io
+
+// func uploadFiles() {
+// 	// this function should updload files to icommon.io
+// }
+
+// func downloadFiles() {
+// 	// this function should download files from iconmoon.io
+// }
