c560db384bcc2446613c6ec185530d2835dd5769
Pedro Santo <pedro.bb.90@gmail.com>
2026-07-31 17:17:01 +0100

    added base functions

--- main.go
+++ main.go
@@ -7,10 +7,27 @@ import (
 
 func main() {
 	fmt.Println("Welcome SVG Auto!")
-	getFiles()
+	
+	fmt.Println(getFiles())
 }
 
-func getFiles() {
-	args := os.Args
-	fmt.Println("Args:", args[1:])
+func getFiles() []string {
+	var args []string
+	
+	for _, arg := range os.Args[1:] {
+		args = append(args, arg)
+	}
+	return args
+}
+
+func accessIcoMoon() {
+	// this function should access icomoon.io
+}
+
+func uploadFiles() {
+	// this function should updload files to icommon.io
+}
+
+func downloadFiles() {
+	// this function should download files from iconmoon.io
 }
